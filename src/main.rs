70c6e563def661e2605df025ca1fbc23dad84764
IsmaelJC <ismael@bemmbo.com>
2025-10-31 16:34:04 -0300

    Implemented From<&[u8; 12]> for DnsHeader

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,48 @@ struct DnsHeader {
     additional_record_count: u16,
 }
 
+impl From<&[u8; 12]> for DnsHeader {
+    fn from(buf: &[u8; 12]) -> Self {
+        Self {
+            packet_identifier: u16::from_be_bytes([buf[0], buf[1]]),
+            query_response_indicator: (buf[2] & 0b10000000) != 0,
+            operation_code: (buf[2] & 0b01111000) >> 3,
+            authoritative_answer: (buf[2] & 0b00000100) != 0,
+            truncation: (buf[2] & 0b00000010) != 0,
+            recursion_desired: (buf[2] & 0b00000001) != 0,
+            recursion_available: (buf[3] & 0b10000000) != 0,
+            reserved: (buf[3] & 0b01110000) >> 4,
+            response_code: buf[3] & 0b00001111,
+            question_count: u16::from_be_bytes([buf[4], buf[5]]),
+            answer_record_count: u16::from_be_bytes([buf[6], buf[7]]),
+            authority_record_count: u16::from_be_bytes([buf[8], buf[9]]),
+            additional_record_count: u16::from_be_bytes([buf[10], buf[11]]),
+        }
+    }
+}
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; 512];
+
+    // let header = DnsHeader {
+    //     packet_identifier: 1234,
+    //     query_response_indicator: true,
+    //     operation_code: 0,
+    //     authoritative_answer: false,
+    //     truncation: false,
+    //     recursion_desired: false,
+    //     recursion_available: false,
+    //     reserved: 0,
+    //     response_code: 0,
+    //     question_count: 0,
+    //     answer_record_count: 0,
+    //     authority_record_count: 0,
+    //     additional_record_count: 0,
+    // };
 
     loop {
         match udp_socket.recv_from(&mut buf) {
