dfbf5cd47a1a1bf31448cbf0fdfdc52a86267bff
IsmaelJC <ismael@bemmbo.com>
2025-10-31 19:12:50 -0300

    Added enum for question class, added conversion from u16 and added tests

--- src/dns/question.rs
+++ src/dns/question.rs
@@ -48,6 +48,32 @@ impl TryFrom<u16> for RecordType {
     }
 }
 
+/// DNS Class types as defined in RFC 1035 section 3.2.4.
+///
+/// This enum represents the CLASS field in a DNS question or resource record,
+/// indicating the protocol family (such as Internet, Chaos, etc.) being used.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Class {
+    IN = 1,
+    CS = 2,
+    CH = 3,
+    HS = 4,
+}
+
+impl TryFrom<u16> for Class {
+    type Error = ();
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(Class::IN),
+            2 => Ok(Class::CS),
+            3 => Ok(Class::CH),
+            4 => Ok(Class::HS),
+            _ => Err(()),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -73,6 +99,18 @@ mod tests {
         // Test error case
         assert_eq!(RecordType::try_from(0), Err(()));
         assert_eq!(RecordType::try_from(17), Err(()));
-        assert_eq!(RecordType::try_from(65535), Err(()));
+        assert_eq!(RecordType::try_from(200), Err(()));
+    }
+
+    #[test]
+    fn test_class_conversion() {
+        assert_eq!(Class::try_from(1), Ok(Class::IN));
+        assert_eq!(Class::try_from(2), Ok(Class::CS));
+        assert_eq!(Class::try_from(3), Ok(Class::CH));
+        assert_eq!(Class::try_from(4), Ok(Class::HS));
+        // Test error case
+        assert_eq!(Class::try_from(0), Err(()));
+        assert_eq!(Class::try_from(5), Err(()));
+        assert_eq!(Class::try_from(123), Err(()));
     }
 }
